5d175defde49a117f57ce3135d1f23f481795491
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-30 00:12:39 +0100

    Update of cache

--- cache/UpdateCache.sh
+++ cache/UpdateCache.sh
@@ -2,7 +2,7 @@
 
 wget -O  Natuurgebieden.json 'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4);way["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4);relation["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4););out body;>;out skel qt;'
 
-wget -O Zuidleie.json 'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4);way["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4);relation["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4););out body;>;out skel qt;'
+wget -O Zuidleie.json 'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["operator"~"Natuurpunt Vallei van de Zuidleie"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4);way["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4);relation["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4););out body;>;out skel qt;'
 
 git add .
 git commit -m "Update of cache"
