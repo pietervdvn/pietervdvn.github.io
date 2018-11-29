80f87396a8d9d83a425ad678f33f1210ce48cfbf
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-30 00:09:56 +0100

    Update of cache

--- cache/UpdateCache.sh
+++ cache/UpdateCache.sh
@@ -2,6 +2,8 @@
 
 wget -O  Natuurgebieden.json 'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4);way["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4);relation["operator"="Natuurpunt Brugge"]["leisure"="nature_reserve"](49.5,2.367,51.683,6.4););out body;>;out skel qt;'
 
-git add Natuurgebieden.json
+wget -O Zuidleie.json 'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4);way["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4);relation["operator"~"Natuurpunt Vallei van de Zuidleie"]["tourism"="information"](49.5,2.367,51.683,6.4););out body;>;out skel qt;'
+
+git add .
 git commit -m "Update of cache"
 git push
