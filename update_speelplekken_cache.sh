a1a1f5762212f0236673dd7a574cbd54ad769e63
Pieter Vander Vennet <pietervdvn@posteo.net>
2021-04-26 21:54:06 +0200

    Update of the speelplekken cache

--- update_speelplekken_cache.sh
+++ update_speelplekken_cache.sh
@@ -2,6 +2,7 @@
 echo "cd pietervdvn.github.io"
 date
 whoami
+PATH=/home/pietervdvn/.local/bin:/home/pietervdvn/.nvm/versions/node/v16.0.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pietervdvn/.dotnet/tools
 cd /home/pietervdvn/git/pietervdvn.github.io
 rm speelplekken_cache/*.json
 cd ../MapComplete
