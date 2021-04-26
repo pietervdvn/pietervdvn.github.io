fcd2d127abf4258de1c650d23a78074c9d76aa22
Pieter Vander Vennet <pietervdvn@posteo.net>
2021-04-26 16:00:09 +0200

    Update of the speelplekken cache

--- update_speelplekken_cache.sh
+++ update_speelplekken_cache.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-
+echo "cd pietervdvn.github.io"
 cd /home/pietervdvn/git/pietervdvn.github.io
 rm speelplekken_cache/*.json
 cd ../MapComplete
 git pull
+echo "Starting the caching script"
 npm run generate:cache:speelplekken
-
+echo "Caching done, commiting now"
 cd ../pietervdvn.github.io
 git pull
 git commit -am "Update of the speelplekken cache"
+echo "All done!"
