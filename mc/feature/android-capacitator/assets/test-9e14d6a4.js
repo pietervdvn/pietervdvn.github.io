53e953e76b89429f5b4aacf477451363d48cf2d7
pietervdvn <pietervdvn@posteo.net>
2024-11-18 20:02:50 +0000

    Deploying a new version of mapcomplete

---
+++ mc/feature/android-capacitator/assets/test-9e14d6a4.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-3cfb730f.js";import{S as e,i as n,s}from"./index-4cd424a8.js";class o extends e{constructor(t){super(),n(this,t,null,null,s,{})}}new o({target:document.getElementById("maindiv")});
+//# sourceMappingURL=test-9e14d6a4.js.map
--- mc/feature/android-capacitator/assets/test-a8d5cf0b.js
+++
@@ -1,2 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";import{S as e,i as n,s}from"./index-f635bd5d.js";class o extends e{constructor(t){super(),n(this,t,null,null,s,{})}}new o({target:document.getElementById("maindiv")});
-//# sourceMappingURL=test-a8d5cf0b.js.map
