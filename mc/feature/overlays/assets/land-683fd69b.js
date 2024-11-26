f439c9405ae203a1032d01643c453506d320f49b
pietervdvn <pietervdvn@posteo.net>
2024-11-26 21:36:56 +0000

    Deploying a new version of mapcomplete

---
+++ mc/feature/overlays/assets/land-683fd69b.js
@@ -0,0 +1,2 @@
+import"./UIEventSource-e54bf5d1.js";import{O as i}from"./OsmConnection-eb783479.js";import"./Constants-d7cb9d12.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-683fd69b.js.map
