b589182216dfa405a76d9a42ceaba6a2521afe8b
pietervdvn <pietervdvn@posteo.net>
2024-12-01 23:57:37 +0000

    Deploying a new version of mapcomplete

---
+++ mc/theme/insect-hotels/assets/land-cb61e1e2.js
@@ -0,0 +1,2 @@
+import"./UIEventSource-e54bf5d1.js";import{O as i}from"./OsmConnection-d2ac0727.js";import"./Constants-93cd77aa.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-cb61e1e2.js.map
